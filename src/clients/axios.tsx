b06fae8dc0ab58459116a613eed2d92ad7ff320f
Axel <axel.mondaca@alumnos.ucn.cl>
2025-06-15 13:05:05 -0400

    feat: Configure axios for connection with backend

---
+++ src/clients/axios.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+/**
+ * Axios instance for API backend requests.
+ * This instance is configured with the base URL and default headers.
+ */
+
+const ApiBackend = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BACKEND_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+export { ApiBackend };
